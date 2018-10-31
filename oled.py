47b2e0861d695c28ed0a6eff8ef05f8d35f972c6
wuyue <wuyue96@gmail.com>
2018-10-31 15:33:21 +0800

    添加了oled.py和startEMS.py

--- oled.py
+++ oled.py
@@ -8,49 +8,49 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 class oledDisplay():
-    
-    # Raspberry Pi pin configuration:
-    RST = 24
-    # Note the following are only used with SPI:
-    DC = 23
-    SPI_PORT = 0
-    SPI_DEVICE = 0
+    def __init__(self):
+        # Raspberry Pi pin configuration:
+        RST = 24
+        # Note the following are only used with SPI:
+        DC = 23
+        SPI_PORT = 0
+        SPI_DEVICE = 0
 
-    # Beaglebone Black pin configuration:
-    # RST = 'P9_12'
-    # Note the following are only used with SPI:
-    # DC = 'P9_15'
-    # SPI_PORT = 1
-    # SPI_DEVICE = 0
+        # Beaglebone Black pin configuration:
+        # RST = 'P9_12'
+        # Note the following are only used with SPI:
+        # DC = 'P9_15'
+        # SPI_PORT = 1
+        # SPI_DEVICE = 0
 
-    # 128x32 display with hardware I2C:
-    disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+        # 128x32 display with hardware I2C:
+        disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-    # Initialize library.
-    disp.begin()
+        # Initialize library.
+        disp.begin()
 
-    # Clear display.
-    disp.clear()
-    disp.display()
+        # Clear display.
+        disp.clear()
+        disp.display()
 
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    width = disp.width
-    height = disp.height
-    image = Image.new('1', (width, height))
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        width = disp.width
+        height = disp.height
+        image = Image.new('1', (width, height))
 
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
 
-    # First define some constants to allow easy resizing of shapes.
-    padding = 2
-    top = padding
-    bottom = height-padding
-    # Move left to right keeping track of the current x position for drawing shapes.
-    x = padding
+        # First define some constants to allow easy resizing of shapes.
+        padding = 2
+        top = padding
+        bottom = height-padding
+        # Move left to right keeping track of the current x position for drawing shapes.
+        x = padding
 
-    # Load default font.
-    font = ImageFont.load_default()
+        # Load default font.
+        font = ImageFont.load_default()
 
     # Write two lines of text.
     draw.text((x, top), '', font=font, fill=255)
---
+++ startEMS.sh
@@ -0,0 +1,1 @@
+nohup python3 main.py >> 1.log 2>>&1 &
