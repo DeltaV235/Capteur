2dbef592b2fda921918eb08a764663cf3bd61073
DeltaV235 <wuyue96@gmail.com>
2019-07-29 16:37:16 +0800

    install_env.sh: 重构了apt包检查与安装的代码，使其可以更统一的管理需要安装的包，增加复用的便利性

--- install_env.sh
+++ install_env.sh
@@ -5,54 +5,71 @@
 #   2019/07/28  DeltaV235   V1.0
 
 echo "Checking python environment ..."
-aptList=$(apt list 2>/dev/null|grep "installed"|cut -d '/' -f1)
-isInstalledPyt=false
-isInstalledPip=false
-for line in $aptList
+
+aptInsList="python3.7 python3-pip"          # 需要安装的包名
+aptShouldIns=""                             # 将要被安装的包名
+
+# 判断那些包需要被安装
+for package in $aptInsList
 do
-#    echo "$line"
-    if [ "$line" == "python3.7" ];then
-        isInstalledPyt=true
-    elif [ "$line" == "python3-pip" ];then
-        isInstalledPip=true
-    fi
-    if [ $isInstalledPyt == true ] && [ $isInstalledPip == true ]; then
-        echo -e "\npython\t\tversion:  $(apt list python3.7 2>/dev/null|grep "\[installed\]"|cut -d " " -f2)"
-        echo -e "pip3\t\tversion:  $(apt list python3-pip -a 2>/dev/null|grep "\[installed\]"|cut -d " " -f2)"
-        break
+    if [ "$(apt list "$package" 2>/dev/null|grep "\[installed\]"|cut -d "/" -f1)" != "$package" ];then
+        aptShouldIns="$aptShouldIns $package"
     fi
 done
-if [ $isInstalledPyt == false ];then
-    echo -e "Installing python3.7\n" && sleep 2
-    sudo apt-get install python3.7-minimal -y
-fi
-if [ $isInstalledPip == false ];then
-    echo -e "Installing python3-pip\n" && sleep 2
-    sudo apt-get install python3-pip -y
-fi
+
+# 若首次因为网络原因安装失败，则重试3次后依然失败，则提示检查网络或手动安装相应的包,并返回255
+while true
+do
+    declare -i count=0
+    if [ "$aptShouldIns" != "" ];then
+        echo "These package(s) will be installed:"
+        echo "$pipShouldIns" && sleep 3
+        sudo apt-get install -y "$pipShouldIns"
+        if [ "$?" == true ];then
+            printf "\n\033[1;32m%s\033[0m\n\n" "Package install SUCC"
+            break
+        else
+            printf "\n\033[1;31m%s\033[0m\n\n" "Package install FAIL"
+            if [ $count -lt 4 ];then
+                echo -e "Reinstalling(three times) ...\n"
+            else
+                echo -e "Please check your network or install these package manually\n"
+                echo -e "\t$aptShouldIns\n"
+                exit 255
+            fi
+        fi
+    fi
+    (count++)
+done
+
+# 若安装成功或无需安装，则输出对应包的版本
+for package in $aptInsList
+do
+        printf "%-30sversion: %s" "$package" "$(apt list "$package" 2>/dev/null|grep "\[installed\]"|cut -d " " -f2)"
+done
 
 echo -e "\nPython check complete"
 echo "---------------------------------------------------"
 echo -e "\nChecking python3 package environment ...\n"
 
-installList="Adafruit-BMP Adafruit-DHT Adafruit-GPIO Adafruit-PureIO Adafruit-SSD1306 coloredlogs PyMySQL smbus RPi.GPIO pillow"
-installedPackage=$(pip3 list|awk 'NR>2{printf "%s\n",$0}')
-#echo "$installedPackage"
-shouldInstall=""
-for list in $installList
+pipInsList="Adafruit-BMP Adafruit-DHT Adafruit-GPIO Adafruit-PureIO Adafruit-SSD1306 coloredlogs PyMySQL smbus RPi.GPIO pillow"
+pipInsPackage=$(pip3 list|awk 'NR>2{printf "%s\n",$0}')
+#echo "$pipInsPackage"
+pipShouldIns=""
+for package in $pipInsList
 do
-    if [ "$(echo "$installedPackage"|grep "$list")" == "" ];then
-        shouldInstall="$shouldInstall $list"
+    if [ "$(echo "$pipInsPackage"|grep "$package"|cut -d " " -f1)" != "$package" ];then
+        pipShouldIns="$pipShouldIns $package"
     fi
 done
 
-#echo "$shouldInstall"
+#echo "$pipShouldIns"
 while true
 do
-    if [ "$shouldInstall" != "" ];then
+    if [ "$pipShouldIns" != "" ];then
         echo "This package will be installed:"
-        echo "$shouldInstall"
-        sudo pip3 install "$shouldInstall"
+        echo "$pipShouldIns"
+        sudo pip3 install "$pipShouldIns"
         if [ "$?" == true ];then
             printf "\n\033[1;32m%s\033[0m\n" "Install package SUCC"
             break
@@ -63,13 +80,13 @@ do
     fi
 done
     printf "%-30s%s\n\n" "package" "version"
-    for list in $installList
+    for list in $pipInsList
     do
-        echo "$installedPackage"|grep "$list"|awk '{printf "%-30s%s\n",$1,$2}'
+        echo "$pipInsPackage"|grep "$list"|awk '{printf "%-30s%s\n",$1,$2}'
     done
 
 
 echo
-python3 test.py && printf "\033[32m%s\033[0m\n\n" "TEST PASS" || printf "\033[31m%s\033[0m\n\n" "TEST FAIL"
+python3 test.py && printf "\033[1;32m%s\033[0m\n\n" "TEST PASS" || printf "\033[1;31m%s\033[0m\n\n" "TEST FAIL"
 #uninstallPackage=""
-#for package in $installedPackage
+#for package in $pipInsPackage
